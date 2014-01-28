0dfcbfd19186b4de53a881fb8d56fe4de6ad992d
Antonn Esquivel <esquivel.antonn@gmail.com>
2014-01-29 01:20:50 +0800

    Hero, Video, Blog, Contact

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 $(function() {
+
 	var maincontainer = "#maincontainer";
+
 
 	// Initialize one page scroll
 	$(maincontainer).onepage_scroll({
@@ -8,10 +10,14 @@ $(function() {
 		animationTime: 1000,
 		pagination: true,
 		updateURL: false,
-		beforeMove: function(newindex) {
-			highlight_toplink(newindex - 1);
+		beforeMove: function(index) {
+			updateState(index);
+		},
+		afterMove: function(index) {
+			// TO DO: Play vid pagination
 		}
 	});
+
 
 	// Nav
 	$('a#top-logo').click(function(index){
@@ -20,18 +26,111 @@ $(function() {
 	});
 	$('nav#top-nav ul li').each(function(index){
 		$(this).find('a').click(function(){
-			$(maincontainer).moveTo(index + 2);
+			$(maincontainer).moveTo(index + 3);
 			return false;
 		});
 	});
+
 
 	// Triggers
 	$('.pane-1 .bottombutton button').click(function(){
 		$(maincontainer).moveDown();
 	});
+
+
+	// Video
+	var videoPlayer = videojs("#senti-vid");
+	if (videoPlayer) {
+		videoPlayer.volume(0.6);
+	}
+
+
+	// Google Maps
+	init_maps();
+
+
+	// Test
+	// $(maincontainer).moveTo(6);
+
 });
 
-function highlight_toplink (index) {
+
+
+// Update website state depending on the current panel
+function updateState (index) {
+	// Highlight link
 	$('nav#top-nav ul li a').removeClass('active');
-	$('nav#top-nav ul li:nth-child('+index+') a').addClass('active');
+	$('nav#top-nav ul li:nth-child('+ (index - 2) +') a').addClass('active');
+
+	// Make header transparent
+	if (index == 1 || index == 2 || index == 6) {
+		$('header#top').addClass('transparent');
+	} else if ($('header#top').hasClass('transparent')) {
+		$('header#top').removeClass('transparent');
+	}
+
+	// Play/pause video
+	var videoPlayer = videojs("#senti-vid");
+	if (videoPlayer) {
+		if (index == 2) {
+			videoPlayer.play();
+		} else {
+			videoPlayer.pause();
+		}
+	}
+}
+
+
+// Google Maps
+function init_maps () {
+	var latLng = new google.maps.LatLng(14.6222357, 121.0565551);
+	var myOptions = {
+		panControl: false,
+		zoomControl: false,
+		mapTypeControl: true,
+		scaleControl: false,
+		streetViewControl: false,
+		overviewMapControl: true,
+		draggable: true,
+		disableDoubleClickZoom: true,
+		scrollwheel: false,
+		zoom: 17,
+		center: latLng,
+		mapTypeId: google.maps.MapTypeId.ROADMAP
+	};
+	map = new google.maps.Map($("div#bgmap")[0], myOptions);
+
+	var markerlatlng = new google.maps.LatLng(14.6222357, 121.0565551);
+	var marker = new google.maps.Marker({
+		position: markerlatlng,
+		title: "Senti Main Office",
+		animation: google.maps.Animation.DROP
+	});
+	marker.setMap(map);
+
+
+	var boxText = " \
+		<div id='maptooltip'> \
+			<h1>SENTI Main Office</h1> \
+			<address>Cubao Expo, General Romulo Street<br />Araneta Center, Cubao</address> \
+			<h2>contactus <span>@</span> senti.com.ph</h2> \
+		</div> \
+	";
+
+	var myOptions = {
+		content: boxText,
+		disableAutoPan: false,
+		maxWidth: 0,
+		pixelOffset: new google.maps.Size(-175, 0),
+		zIndex: null,
+		boxStyle: {},
+		closeBoxMargin: "25px 5px 0 0",
+		infoBoxClearance: new google.maps.Size(1, 1),
+		isHidden: false,
+		pane: "floatPane",
+		enableEventPropagation: false
+	};
+
+	var ib = new InfoBox(myOptions);
+	ib.open(map, marker);
 }
